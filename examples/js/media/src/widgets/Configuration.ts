90c59e5fd1a49de7a55269e2d06534aa8c50dbb9
sky428 <119054774+sky428@users.noreply.github.com>
2022-12-12 14:28:53 +0000

    Update Configuration.ts

--- examples/js/media/src/widgets/Configuration.ts
+++ examples/js/media/src/widgets/Configuration.ts
@@ -1,6 +1,7 @@
 import { configure } from 'instantsearch.js/es/widgets';
 
 export const configuration = configure({
-  attributesToSnippet: ['content:20'],
+  //attributesToSnippet: ['content:20'],
+  attributesToSnippet: ['full_text:20'],
   snippetEllipsisText: '…',
 });
